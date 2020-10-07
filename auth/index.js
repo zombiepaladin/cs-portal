478fe4ad9eaa2368efad8a6da361c4d72c335aa6
Nathan Bean <nhbean@ksu.edu>
2020-10-07 09:22:46 -0500

    Added user name retrieval

--- auth/index.js
+++ auth/index.js
@@ -1,7 +1,7 @@
 /** CAS authentication express subapp */
 
 // Requires
-const https = require('https');
+const axios = require('axios');
 const express = require('express');
 const process = require('process');
 const {findOrCreateUser} = require('./user-functions');
@@ -26,13 +26,12 @@ app.get('/auth/ticket', ticket);
 
 // login required middleware
 app.loginRequired = (req, res, next) => {
-  console.log(req.url);
   // Allow routes starting with /auth/ through
   if(/$\/auth\//.test(req.url)) return next();
   // If no user is set on the session, redirect to the login url
-  if(!req.session.user) {console.log('no user'); res.redirect('/auth/login');}
+  if(!req.session.user) res.redirect('/auth/login');
   // Otherwise, allow the request to move forward
-  else {console.log("in"); next();}
+  else next();
 };
 
 module.exports = app;
@@ -65,54 +64,26 @@ function logout(req, res) {
   * @param {http.IncomingRequest} req - the request object
   * @param {http.ServerResponse} res - the response object
   */
-function ticket(req, res) {
+async function ticket(req, res) {
   // get the ticket from the querystring
   var ticket = req.query.ticket;
   // We need to verify this ticket with the CAS server,
   // by making a request against its serviceValidate url
   var url = casHost + 'serviceValidate?ticket=' + ticket + '&service=' + serviceHost + "ticket";
-  https.get(url, function(response){
-    var body = "";
-    // The request body will come in chunks; we
-    // must collect it
-    response.on('data', function(chunk){
-      body += chunk;
-    });
-    // Once it's collected, we want to see if
-    // it contains a success or failure message
-    response.on('end', function(){
-      console.log(body);
-      // The contents are XML, and we can look
-      // for a <cas:user>username</cas:user>
-      // element if our user logged in successfully
-      var match = /<cas:user>(\S+)<\/cas:user>/.exec(body);
-      if(match) {
-        // if we have a match, the user logged in through CAS;
-        // Find the user and create a session
-        var eid = match[1];
-console.log('eid:', eid);
-console.log(match);
-        var db = req.app.get('db');
-        findOrCreateUser(db, eid).then(user => {
-console.log("user:", user);
-          req.session.user = user;
-          res.redirect('/');
-        }).catch(err => {
-          console.error(err);
-          res.statusCode = 500;
-          res.end("Server Error");
-        });
-
-      } else {
-        // If there is no match, the CAS server rejected
-        // the token.
-        res.statusCode = 403;
-        res.end("Unauthorized");
-      }
-    });
-  }).on('error', function(err) {
-    console.error(err);
-    res.statusCode = 500;
-    res.end();
-  });
+  var response = await axios.get(url);
+  var match = /<cas:user>(\S+)<\/cas:user>/.exec(response.data);
+  if(match) {
+    // if we have a match, the user logged in through CAS;
+    // Find the user and create a session
+    var eid = match[1];
+    var db = req.app.get('db');
+    var user = await findOrCreateUser(db, eid);
+    req.session.user = user;
+    res.redirect('/');
+  } else {
+    // If there is no match, the CAS server rejected
+    // the token.
+    res.statusCode = 403;
+    res.end("Unauthorized");
+  }
 }
--- auth/user-functions.js
+++ auth/user-functions.js
@@ -1,27 +1,24 @@
 // Requires
-const https = require('https');
 const { resolve } = require('path');
+const axios = require('axios');
 
 /** @function findOrCreateUser
  * Finds the user in the database with the specified eid,
  * or creates the user if they do not exist
  * @param {Database} db - the database to add the user to
- * @param {string} eid - the user's K-State eid 
+ * @param {string} eid - the user's K-State eid
  * @returns {Promise} resolves to a user object
  */
 async function findOrCreateUser(db, eid) {
-    console.log("In Find Or Create User", eid);
     var user = await findUser(db, eid);
-    console.log('1', user);
     if(!user) user = await createUser(db, eid);
-    console.log('2', user);
     return user;
 }
 
 /** @function findUser
  * Finds the user in the database with the specified eid
  * @param {Database} db - the database to add the user to
- * @param {string} eid - the user's K-State eid 
+ * @param {string} eid - the user's K-State eid
  * @returns {Promise} resolves to a user object
  */
 async function findUser(db, eid) {
@@ -33,38 +30,22 @@ async function findUser(db, eid) {
  * Creates a user entry in the database for the supplied eid,
  * Adding directory information from the K-State SOAP service
  * @param {Database} db - the database to add the user to
- * @param {string} eid - the user's K-State eid 
+ * @param {string} eid - the user's K-State eid
  * @returns {Promise} resolves to a user object
  */
 async function createUser(db, eid) {
-    return new Promise((resolve, reject) => {
-        // Request user directory information from K-State SOAP service
-        var url = `https://www.k-state.edu/People/filter/eid=${eid}`;
-        https.get(url, (response) => {
-            var body = "";
-            // The request body will come in chunks; we
-            // must collect it
-            response.on('data', (chunk) => {
-                body += chunk;
-            });
-            // Once it's collected, we get the user's info
-            response.on('end', () => {
-                var first = /<fn>(\S+)<\/fn>/.exec(body)[1];
-                var last = /<ln>(\S+)<\/ln>/.exec(body)[1];
-                console.log('first', first, 'last', last);
-                db.users.save({
-                    eid: eid,
-                    first: first,
-                    last: last
-                }).then(user => {
-                    console.log("ONCE MORE USER:", user);
-                    resolve(user)
-                }).catch(err => console.error(err));
-            });
-            // If there is an error processing the body, reject
-            response.on('error', reject);
-        });
-    });
+  // Get the user's directory information
+  var url = `https://www.k-state.edu/People/filter/eid=${eid}`;
+  var response = await axios.get(url);
+  // Extract the user's first and last names from it
+  var first = /<fn>(\S+)<\/fn>/.exec(response.data)[1];
+  var last = /<ln>(\S+)<\/ln>/.exec(response.data)[1];
+  // Insert the user into the database, and return them
+  return await db.users.insert({
+      eid: eid,
+      first: first,
+      last: last
+  });
 }
 
 module.exports = {
